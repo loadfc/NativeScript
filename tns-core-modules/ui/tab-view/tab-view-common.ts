560d5ab19ed668abe426fcecc176fabd5908cba6
Vasil Chimev <vasil.chimev@gmail.com>
2019-03-15 11:45:02 +0200

    feat(HMR): preserve selectedIndex of TabView

--- tns-core-modules/ui/tab-view/tab-view-common.ts
+++ tns-core-modules/ui/tab-view/tab-view-common.ts
@@ -160,6 +160,18 @@ export class TabViewBase extends View implements TabViewDefinition, AddChildFrom
         return items ? items.length : 0;
     }
 
+    public _onLivesync(context?: ModuleContext): boolean {
+        super._onLivesync(context);
+
+        const selectedIndex = this.selectedIndex;
+        const items = this.items;
+        const selectedTabViewItem = items[selectedIndex];
+        const view = selectedTabViewItem.view;
+
+        // Handle on `Frame` level
+        return view._onLivesync();
+    }
+
     public eachChild(callback: (child: ViewBase) => boolean) {
         const items = this.items;
         if (items) {
